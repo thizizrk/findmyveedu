9018f3abe0e507e59515da2de7ce38a1d3f87900
thizizrk <ranjithkumarpothiraj@gmail.com>
2024-10-06 03:17:02 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
