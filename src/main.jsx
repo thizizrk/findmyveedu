eab29de8decf28e15853d1f1e1a6d6792d960863
thizizrk <ranjithkumarpothiraj@gmail.com>
2024-10-06 03:35:59 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.scss'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter  basename="/findmyveedu">
+    <HashRouter  basename="/findmyveedu">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
